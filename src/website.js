013a630c0438a72e512a09e680fb41be5493066c
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-05-25 19:20:33 +0200

    add header, body with background img and footer functions

--- src/website.js
+++ src/website.js
@@ -33,8 +33,41 @@ function createNav() {
   return nav;
 }
 
+function createBody() {
+  const mainBody = document.createElement("div");
+  mainBody.classList.add("mainBody");
+
+  const bodyTitle = document.createElement("h1");
+  bodyTitle.innerHTML = "Fancy sweetness overload?";
+  mainBody.appendChild(bodyTitle);
+
+//   const backgroundImg = document.createElement('img');
+//   backgroundImg.classList.add('background');
+//   backgroundImg.src = '../img/background.jpg';
+//   mainBody.appendChild(backgroundImg);
+
+    const bodyText = document.createElement('p');
+    bodyText.classList.add('bodyText')
+    bodyText.innerHTML = 'If you are longing for a sweet place where you can spend some time to boost your energy (and sugar!), look no further. Sweets Factory is here to fill cravings of each and every sweet tooth wandering aorund!'
+    mainBody.appendChild(bodyText);
+  return mainBody;
+}
+
+function createFooter() {
+    const footer = document.createElement('div');
+    footer.classList.add('footer');
+    
+    const signature = document.createElement('h4');
+    signature.innerHTML = 'Mateusz Kuruc 2023';
+    footer.appendChild(signature);
+
+    return footer;
+}
+
 function startWebsite() {
-    const content = document.getElementById('content');
-    content.appendChild(createHeader());
-    content.appendChild(createNav());
+  const content = document.getElementById("content");
+  content.appendChild(createHeader());
+  content.appendChild(createNav());
+  content.appendChild(createBody());
+  content.appendChild(createFooter());
 }
